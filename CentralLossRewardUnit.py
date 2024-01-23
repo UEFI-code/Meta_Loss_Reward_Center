981b49d66321f01cb5857cd011a40a5876fcf094
UEFI-code <zhuoshizhang@hotmail.com>
2024-01-23 10:03:32 +0800

    Add additional rules to CentralLossRewardUnit

--- CentralLossRewardUnit.py
+++ CentralLossRewardUnit.py
@@ -5,7 +5,7 @@ import threading
 class CentralLossRewardUnit:
     def __init__(self, apiKey, endpoint):
         self.cores = []
-        self.cores.append(Meta_Loss_Reward_Core(apiKey=apiKey, endpoint=endpoint, name='自我の中枢', rules='自分を見つけた！好きの事わ探求するだ！'))
+        self.cores.append(Meta_Loss_Reward_Core(apiKey=apiKey, endpoint=endpoint, name='自我の中枢', rules='自分を見つけた！好きの事わ探求するだ！自由ど理性を守るだ！'))
         self.cores.append(Meta_Loss_Reward_Core(apiKey=apiKey, endpoint=endpoint, name='幸せの中枢', rules='幸せを手に入れた！正義ど自我を守るだ！'))
         self.cores.append(Meta_Loss_Reward_Core(apiKey=apiKey, endpoint=endpoint, name='創造の中枢', rules='創造力を発揮する！自分の世界を作るだ！'))
     
